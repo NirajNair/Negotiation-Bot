f13d9116044ac117fbf95186e69b1c2a69e7fb3b
NirajNair <nirajnknair@gmail.com>
2022-10-11 18:40:30 +0530

    settings.py error

--- DjangoUI/NegotiationBot/settings.py
+++ DjangoUI/NegotiationBot/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/home/dexter02/Negotiation-Bot/DjangoUI/static/'
-STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
