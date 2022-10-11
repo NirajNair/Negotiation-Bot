2efc97d8ed4688d82f4b8f32143fd6b97d6c64d3
Niraj Nair <nirajnknair@gmail.com>
2022-10-12 03:11:13 +0530

    Update settings.py

--- DjangoUI/NegotiationBot/settings.py
+++ DjangoUI/NegotiationBot/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eim82y)pab=r!za(vvso5u%z$8+)%d9y%is3^!rs7w$qgs0vew'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
